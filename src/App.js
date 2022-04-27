8ee025981494bc2c00ee6961b2f26de7e284f572
pachippe1 <patrick.schippers@sofwerx.org>
2022-04-27 19:59:41 -0400

    Adding post project to github.io

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Routing from './Routes';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='listing'>
+        <Routing/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
